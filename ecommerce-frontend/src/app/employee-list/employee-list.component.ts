b182cb2ec4360b1a76df6791878be84e090201c4
Sgarcia8 <sjgarciat@udistrital.edu.co>
2024-08-01 12:25:22 -0500

    Creación proyecto angular y configuración inicial para trabajar con bootstrap

---
+++ ecommerce-frontend/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrl: './employee-list.component.css'
+})
+export class EmployeeListComponent implements OnInit {
+  
+  employees!: Employee[];
+  
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
